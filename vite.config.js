2372f11a48b0524a92b8dd06a33782524636efd5
alvinodaris <alvinodaris14@gmail.com>
2026-02-22 00:19:09 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
+  plugins: [
+    nodePolyfills({
+      include: ['buffer', 'process', 'stream', 'util']
+    })
+  ],
   server: {
     port: 5173
   },
